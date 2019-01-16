c0da6374ba61745125f4063c377f12cbed92fc69
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2019-01-16 10:51:21 +0800

    delete a print

--- quora_insincere_questions_classification/__init__.py
+++ quora_insincere_questions_classification/__init__.py
@@ -7,5 +7,4 @@ email: diqiuzhuanzhuan@gmail.com
 """
 import os
 python_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(python_path)
 os.environ["PYTHONPATH"] = python_path
